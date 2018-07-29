e187425870ff9e571476b449618400499ab08822
AmulyaR <ravipati.amulya@gmail.com>
2018-07-28 21:20:27 -0400

    Adding src and gradlew files from Spring initializer code.

---
+++ src/main/java/com/pillar/babysitter/BabysitterApplication.java
@@ -0,0 +1,12 @@
+package com.pillar.babysitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabysitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabysitterApplication.class, args);
+	}
+}
